ad00ee63ade9418c70c8d70731bac35ef445fb3e
Dan Gyi <daniel.gyi@gmail.com>
2019-10-09 14:02:31 +0100

    set-up for active-storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def update
+    @user = User.find(current_user.id)
+    @user.avatar.attach(user_params)
+  end
+
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password, :ethnicity)
+    params.require(:user).permit(:avatar)
   end
 end
