244cc16f55b16b5820913bb1014eb38f6fa5f08f
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-18 11:42:45 +0100

    fix bug that stops chat input field resetting and clear green on mouseover

--- app/javascript/packs/chatbox_options.js
+++ app/javascript/packs/chatbox_options.js
@@ -16,12 +16,11 @@ $(document).ready(function() {
   $(document).on("click","#chat-card",function(event){
       $('#chatbox-btn').css("background-color", '#f5f5f5');
   });
+  $(document).on("mouseover","#chatbox",function(event){
+      $('#chatbox-btn').css("background-color", '#f5f5f5');
+  });
   $(document).on("click","#send-chat", function(event) {
     console.log('sending');
-    $('#message_body').val('');
-  });
-  $(document).on("click","#message_body", function(event) {
-    console.log('clicked on message_body');
-    console.log(this);
+    $('#chat_message_body').val('');
   });
 });
