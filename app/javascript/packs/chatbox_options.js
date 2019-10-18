467e4558a593f868f923e5938565d90d7a97a24c
Dan Gyi <51231840+DanGyi23@users.noreply.github.com>
2019-10-18 12:09:43 +0100

    Update chatbox_options.js

--- app/javascript/packs/chatbox_options.js
+++ app/javascript/packs/chatbox_options.js
@@ -1,17 +1,6 @@
 console.log("loaded friend_status");
 
 $(document).ready(function() {
-  var status = "red"
-  $(document).on("click",".friend_status",function(event){
-    let buttonID = this.id.replace("status-", "");
-      if (status === "red") {
-        status = "green"
-      } else {
-        status = "red"
-      }
-
-      $(`#status-${buttonID}`).css("color", status);
-  });
 
   $(document).on("click","#chat-card",function(event){
       $('#chatbox-btn').css("background-color", '#f5f5f5');
