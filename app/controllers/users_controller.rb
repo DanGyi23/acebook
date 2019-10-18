a46526eebc5f8794922f81177fdd85e3f7b5dc98
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-18 12:05:57 +0100

    fix bug in users non friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def nonfriends
-    @friends_ids = @friendships.map { |x| x.friend_id }
+    @friends_ids = Friendship.where(user_id: current_user).map { |x| x.friend_id }
     @friends_ids.push(current_user.id)
     @non_friends = User.where.not(id: @friends_ids)
   end
